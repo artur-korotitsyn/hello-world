8f7b5f935695b1998410ac13df66d5e81acd6a43
Artur Korotitsyn <artur_korotitsyn@valentina-db.com>
2013-10-27 15:14:56 +0200

    Add build scripts

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: user
+ *
+ * Created on October 27, 2013, 3:07 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
