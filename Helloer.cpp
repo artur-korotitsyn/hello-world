29e484fb67d7cc29e2cc3bd6c8cab1bc33543dba
Artur Korotitsyn <artur_korotitsyn@valentina-db.com>
2013-10-27 20:29:48 +0200

    Added class Helloer

---
+++ Helloer.cpp
@@ -0,0 +1,18 @@
+/* 
+ * File:   Helloer.cpp
+ * Author: user
+ * 
+ * Created on October 27, 2013, 8:27 PM
+ */
+
+#include "Helloer.h"
+
+Helloer::Helloer() {
+}
+
+Helloer::Helloer(const Helloer& orig) {
+}
+
+Helloer::~Helloer() {
+}
+
---
+++ Helloer.h
@@ -0,0 +1,21 @@
+/* 
+ * File:   Helloer.h
+ * Author: user
+ *
+ * Created on October 27, 2013, 8:27 PM
+ */
+
+#ifndef HELLOER_H
+#define	HELLOER_H
+
+class Helloer {
+public:
+    Helloer();
+    Helloer(const Helloer& orig);
+    virtual ~Helloer();
+private:
+
+};
+
+#endif	/* HELLOER_H */
+
